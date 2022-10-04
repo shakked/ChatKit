d76bfd82275735ef6b53f0b5b62dcb9aeac0c85a
Zachary Shakked <zach@shakd.io>
2022-10-04 14:24:05 -0400

    changed chattextvalidator into a regex only field

--- Sources/ChatKit/Models/Chat Elements/Text Input/ChatTextValidator.swift
+++ Sources/ChatKit/Models/Chat Elements/Text Input/ChatTextValidator.swift
@@ -7,37 +7,19 @@
 
 import Foundation
 
-public protocol ChatTextValidator {
-    var errorMessage: String? { get }
-    func validate(text: String) -> Bool
-}
-
-public struct ChatTextValidatorLength: ChatTextValidator {
-    public let length: Int
-    public let greaterThan: Bool
-    public let errorMessage: String?
-    public init(length: Int, greaterThan: Bool, errorMessage: String?) {
-        self.length = length
-        self.greaterThan = greaterThan
-        self.errorMessage = errorMessage
-    }
+public struct ChatTextValidator: Codable {
+    let regex: String
+    let errorMessage: String?
     
-    public func validate(text: String) -> Bool {
-        if greaterThan {
-            return text.count > length
-        } else {
-            return text.count < length
-        }
-    }
-}
-
-public struct ChatTextValidatorRegex: ChatTextValidator {
-    public let regex: String
-    public let errorMessage: String?
-    public init(regex: String, errorMessage: String) {
+    public init(regex: String, errorMessage: String?) {
         self.regex = regex
         self.errorMessage = errorMessage
         let _ = self.validate(text: "")
+    }
+    
+    public init(json: JSON) {
+        self.regex = json["regex"].stringValue
+        self.errorMessage = json["errorMessage"].string
     }
     
     public func validate(text: String) -> Bool {
@@ -45,19 +27,10 @@ public struct ChatTextValidatorRegex: ChatTextValidator {
         let isValid = test.evaluate(with: text)
         return isValid
     }
-}
-
-public struct ChatTextValidatorEmail: ChatTextValidator {
-    public let errorMessage: String? = "Please enter a valid email address."
     
-    public func validate(text: String) -> Bool {
+    public static func email() -> ChatTextValidator {
         let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-        let emailTest = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
-        let isValid = emailTest.evaluate(with: text)
-        return isValid
-    }
-    
-    public init() {
-        
+        let errorMessage = "Please enter a valid email address."
+        return ChatTextValidator(regex: emailRegEx, errorMessage: errorMessage)
     }
 }
